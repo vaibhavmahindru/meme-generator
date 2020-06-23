cae1035b97f7b261b4936126b613b6c3f42540c1
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-06-24 04:42:52 +0530

    Updates

---
+++ precache-manifest.ba1fc16d5ce514c683b2a2e522e89e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35072679f675567530cf7c08d5dfac9f",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "b5a374317fdd6498709d",
+    "url": "/meme-generator/static/css/main.8e5c0013.chunk.css"
+  },
+  {
+    "revision": "125f56e79ad11c06aaf5",
+    "url": "/meme-generator/static/js/2.06eaaf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.06eaaf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a374317fdd6498709d",
+    "url": "/meme-generator/static/js/main.480637ed.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
